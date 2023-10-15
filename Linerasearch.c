1bf6433f2143ba9fd994b756386914a3acdf0661
PrishitaRayapura <138791236+PrishitaRayapura@users.noreply.github.com>
2023-10-15 21:26:00 +0530

    Add files via upload

---
+++ Linerasearch.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int linearSearch(int arr[], int size, int target) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == target) {
+            return i; // Element found at index i
+        }
+    }
+    return -1; // Element not found
+}
+
+int main() {
+    int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int target = 5;
+    
+    int result = linearSearch(arr, size, target);
+    
+    if (result != -1) {
+        printf("Element found at index %d\n", result);
+    } else {
+        printf("Element not found\n");
+    }
+    
+    return 0;
+}
+
